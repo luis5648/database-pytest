beffcfc8d56b3eb8eaf1000edb72560469ad2ff4
luis roman <porta5648@gmail.com>
2022-01-10 22:29:26 -0500

    query with like completed.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 
 import sqlite3
-conn = sqlite3.connect()
+conn = sqlite3.connect("ndb.db")
+
+
+param = str(input("ingrese el patron: "))
+
+
+cursor = conn.cursor()
+
+cursor.execute("select * from weas where weacont like ?",['%'+param+'%'])
+
+
+res = cursor.fetchall()
+
+for row in res:
+    print(row)
+
